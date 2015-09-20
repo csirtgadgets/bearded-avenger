c959e79994dc4e41376ce9fa06e1309aba7f200e
Wes Young <wes@barely3am.com>
2015-09-20 19:32:11 -0400

    fixing constants

--- cif/constants.py
+++ cif/constants.py
@@ -1,16 +1,16 @@
 import os.path
 import tempfile
 
-TEMP_DIR = os.path.join(tempfile.gettempdir(), 'cif')
+TEMP_DIR = os.path.join(tempfile.gettempdir())
 
 LOG_FORMAT = '%(asctime)s - %(levelname)s - %(name)s[%(lineno)s] - %(message)s'
 REMOTE = 'http://localhost:5000'
 
-ROUTER_FRONTEND = "ipc://{}".format(os.path.join(TEMP_DIR, 'frontend.ipc')
-ROUTER_GATHERER = 'ipc://{}'.format(os.path.join(TEMP_DIR, 'gatherer.ipc'))
+ROUTER_FRONTEND = "ipc://{}".format(os.path.join(TEMP_DIR, 'frontend.ipc'))
+ROUTER_GATHERER = "ipc://{}".format(os.path.join(TEMP_DIR, 'gatherer.ipc'))
 ROUTER_PUBLISHER = 'ipc://{}'.format(os.path.join(TEMP_DIR, 'publisher.ipc'))
 STORAGE_ADDR = 'ipc://{}'.format(os.path.join(TEMP_DIR, 'storage.ipc'))
-CTRL_ADDR = 'ipc://{}'.format(os.path.join(TEMP_DIR, 'ctrl.ipc')
+CTRL_ADDR = 'ipc://{}'.format(os.path.join(TEMP_DIR, 'ctrl.ipc'))
 
 DEFAULT_CONFIG = ".cif.yml"
 
