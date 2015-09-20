8ac1319b0145d4b159d63b8879e97b0477449c77
Wes Young <wes@barely3am.com>
2015-09-20 19:25:31 -0400

    fixing constants for ipc

--- cif/constants.py
+++ cif/constants.py
@@ -1,15 +1,18 @@
 import os.path
+import tempfile
+
+TEMP_DIR = os.path.join(tempfile.gettempdir())
 
 LOG_FORMAT = '%(asctime)s - %(levelname)s - %(name)s[%(lineno)s] - %(message)s'
 REMOTE = 'http://localhost:5000'
 
-ROUTER_FRONTEND = 'ipc://frontend.ipc'
-ROUTER_GATHERER = 'ipc://gatherer.ipc'
-ROUTER_PUBLISHER = 'ipc://publisher.ipc'
-STORAGE_ADDR = 'ipc://storage.ipc'
-CTRL_ADDR = 'ipc://ctrl.ipc'
+ROUTER_FRONTEND = "ipc://{}".format(os.path.join(TEMP_DIR, 'frontend.ipc'))
+ROUTER_GATHERER = "ipc://{}".format(os.path.join(TEMP_DIR, 'gatherer.ipc'))
+ROUTER_PUBLISHER = 'ipc://{}'.format(os.path.join(TEMP_DIR, 'publisher.ipc'))
+STORAGE_ADDR = 'ipc://{}'.format(os.path.join(TEMP_DIR, 'storage.ipc'))
+CTRL_ADDR = 'ipc://{}'.format(os.path.join(TEMP_DIR, 'ctrl.ipc'))
 
-DEFAULT_CONFIG = ".cifv3.yml"
+DEFAULT_CONFIG = ".cif.yml"
 
 VERSION = '3.0.0a1'
 
