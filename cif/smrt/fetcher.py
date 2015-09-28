911de7df861540c158cb8082ace18ae4d23b0e19
Wes Young <wes@barely3am.com>
2015-09-28 19:39:30 -0400

    re-factoring fetcher

--- cif/smrt/fetcher.py
+++ cif/smrt/fetcher.py
@@ -1,20 +1,16 @@
 import logging
 import subprocess
-import os.path
 import os
-from cif.smrt.fetcher import Fetcher, LIMIT
 from pprint import pprint
 
 from cif.constants import VERSION
 
-import sys
 import magic
 import re
-from zipfile import ZipFile
 RE_SUPPORTED_DECODE = re.compile("zip|lzf|lzma|xz|lzop")
 
 
-class Http(Fetcher):
+class Fetcher(object):
 
     def __init__(self, rule, feed, cache='var/smrt/', logger=logging.getLogger(__name__)):
 
@@ -22,6 +18,7 @@ class Http(Fetcher):
         self.feed = feed
         self.rule = rule
         self.cache = cache
+        self.fetcher = rule.fetcher
 
         if self.rule.defaults.get('remote'):
             self.remote = self.rule.defaults.get('remote')
@@ -44,9 +41,15 @@ class Http(Fetcher):
         # http://www-archive.mozilla.org/build/revised-user-agent-strings.html
         self.ua = "User-Agent: cif-smrt/{0} (csirtgadgets.org)".format(VERSION)
 
-    def process(self, split="\n", limit=LIMIT):
-        # using wget until we can find a better way to mirror files in python
-        subprocess.check_call(['wget', '--header', self.ua, '--quiet', '-c', self.remote, '-O', self.cache])
+    def process(self, split="\n", limit=0):
+        if self.fetcher == 'http':
+            # using wget until we can find a better way to mirror files in python
+            subprocess.check_call(['wget', '--header', self.ua, '--quiet', '-c', self.remote, '-O', self.cache])
+        else:
+            if self.fetcher == 'file':
+                self.cache = self.remote
+            else:
+                raise NotImplementedError
 
         ftype = magic.from_file(self.cache, mime=True)
         self.logger.debug(ftype)
@@ -61,6 +64,3 @@ class Http(Fetcher):
         #         for m in f.infolist():
         #             while True:
         #                 yield f.read(m.filename).split(split)[0:limit]
-
-
-Plugin = Http
