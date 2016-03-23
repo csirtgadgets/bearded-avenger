5a830f70bc4bbd41d27a9a4e8442dcabc48e0168
Wes Young <wes@barely3am.com>
2016-03-23 12:34:52 -0400

    dns resolver catch

--- cif/utils/__init__.py
+++ cif/utils/__init__.py
@@ -101,13 +101,14 @@ def resolve_itype(indicator):
 
 import dns.resolver
 from dns.resolver import NoAnswer, NXDOMAIN
+from dns.name import EmptyLabel
 def resolve_ns(data, t='A'):
     try:
         answers = dns.resolver.query(data, t)
         resp = []
         for rdata in answers:
             resp.append(rdata)
-    except (NoAnswer, NXDOMAIN):
-        return None
+    except (NoAnswer, NXDOMAIN, EmptyLabel):
+        return []
 
     return resp
