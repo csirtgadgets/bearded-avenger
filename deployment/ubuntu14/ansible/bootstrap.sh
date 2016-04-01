4677e2bc73a7afec83a193741c64e044e36278fa
Wes Young <wes@barely3am.com>
2016-04-01 15:42:37 -0400

    deployment fixes

--- deployment/ubuntu14/ansible/bootstrap.sh
+++ deployment/ubuntu14/ansible/bootstrap.sh
@@ -3,10 +3,19 @@
 set -e
 
 echo 'updating apt-get tree and installing python-pip'
-sudo apt-get update && sudo apt-get install -y python-pip python-dev
+sudo apt-get update && sudo apt-get install -y python-pip python-dev git
 
 echo 'installing ansible...'
-sudo pip install ansible==1.9.4
+sudo pip install ansible==1.9.4 versioneer
+
+echo 'cleaning up dist..'
+rm -rf dist/*.tar.gz
+
+echo 'building dist'
+python setup.py sdist
+
+echo 'moving sdist into ansible dir..'
+cp -f dist/bearded-avenger-*.tar.gz deployment/ubuntu14/ansible/roles/bearded-avenger/files/bearded-avenger3.tar.gz
 
 echo 'running ansible...'
 ansible-playbook -i "localhost," -c local deployment/ubuntu14/ansible/localhost.yml
