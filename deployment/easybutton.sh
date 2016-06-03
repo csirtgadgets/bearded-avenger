271a376a21fdc2a09ea809452f746c33d2d3a2a1
Wes Young <wes@barely3am.com>
2016-06-03 16:01:39 -0400

    cleaning up deployment, centos7 support

--- deployment/easybutton.sh
+++ deployment/easybutton.sh
@@ -46,11 +46,8 @@ case $OS in
     	if [ $VER == "14.04" ]; then
     	    cd deployment/ubuntu14
     	    bash bootstrap.sh
-    	elif [ $VER == '16.04' ]; then
-    	    cd deployment/ubuntu16
-    	    bash bootstrap.sh
     	else
-    	    echo "Currently only 14.04 or 16.04 LTS is supported"
+    	    echo "Currently only 14.04 LTS is supported"
     		echo "We accept Pull Requests! =)"
        	fi
        	;;
@@ -72,7 +69,8 @@ case $OS in
 
     "CentOS Linux" )
         if [ $VER == '7' ]; then
-            bash deployment/centos71/bootstrap.sh
+            cd deployment/centos7
+            bash bootstrap.sh
         else
             echo 'only CentOS 7 is supported'
             echo "We accept Pull Requests! =)"
