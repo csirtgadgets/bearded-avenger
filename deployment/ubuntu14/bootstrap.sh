9ec411e1884d7b30d550b59cee8613782b610fe4
Wes Young <wes@barely3am.com>
2016-06-09 08:18:33 -0400

    fixes #67

--- deployment/ubuntu14/bootstrap.sh
+++ deployment/ubuntu14/bootstrap.sh
@@ -5,7 +5,7 @@ set -e
 echo 'yes' | sudo add-apt-repository 'ppa:fkrull/deadsnakes-python2.7'
 
 echo 'updating apt-get tree and installing python-pip'
-sudo apt-get update && sudo apt-get install -y python2.7 python-pip python-dev git libffi-dev libssl-dev sqlite3
+sudo apt-get update && sudo apt-get install -y python2.7 python-pip python-dev git libffi-dev libssl-dev sqlite3 software-properties-common
 
 echo 'installing ansible...'
 sudo pip install 'setuptools>=11.3' 'ansible==2.1' versioneer markupsafe
