f361b74a16680556b27b37693f0a0ca33d219e19
Wes Young <wes@barely3am.com>
2016-07-01 14:09:59 -0400

    bugfix

--- deployment/Docker/scripts/cif-entrypoint.sh
+++ deployment/Docker/scripts/cif-entrypoint.sh
@@ -2,19 +2,23 @@
 
 set -e
 
-# Drop root privileges if we are running cif allow the container to be started with `--user`
-if [ "$1" = 'cif' -a "$(id -u)" = '0' ]; then
-	# Change the ownership of /data to cif
-	chown -R cif:cif /data
+export CIF_RUNTIME_PATH=/var/lib/cif
 
-	# create cif tokens
-
-	set -- gosu cif "$@"
-	#exec gosu cif "$BASH_SOURCE" "$@"
-
-
+# create cif tokens
+if [ ! -f /etc/cif/cif.yml ]; then
+    cif-store --token-create-admin /etc/cif/cif.yml
 fi
 
-# As argument is not related to cif, then assume that user wants to run his own process, for example a `bash` shell to
-# explore this image
+if [ ! -f /etc/cif/cif-router.yml ]; then
+    cif-store --token-create-hunter /etc/cif/cif-router.yml
+fi
+
+if [ ! -f /etc/cif/csirtg-smrt.yml ]; then
+    cif-store --token-create-smrt /etc/cif/csirtg-smrt.yml
+fi
+
+chown -R cif:cif /var/lib/cif
+chown -R cif:cif /etc/cif
+chown -R cif:cif /var/log/cif
+
 exec "$@"
