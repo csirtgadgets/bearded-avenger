bc953ab9301fe652168d32f8b05fab9aefb2f0ce
Justin <justin@bouncybouncy.net>
2016-11-25 07:43:12 -0500

    problem: reporttime column is missing an index (#144)
    * problem: reporttime column is missing an index
    
    queries use
    
    ORDER BY indicators.reporttime DESC
    
    and this is slow without this index.
    
    * problem: ip tables missing indexes as well
    
    This speeds up /search for an ip by 10x

--- cif/store/sqlite/indicator.py
+++ cif/store/sqlite/indicator.py
@@ -37,7 +37,7 @@ class Indicator(Base):
     asn = Column(Float)
     cc = Column(String)
     protocol = Column(Integer)
-    reporttime = Column(DateTime)
+    reporttime = Column(DateTime, index=True)
     firsttime = Column(DateTime)
     lasttime = Column(DateTime)
     confidence = Column(Float)
@@ -120,7 +120,7 @@ class Ipv4(Base):
     __tablename__ = 'indicators_ipv4'
 
     id = Column(Integer, primary_key=True)
-    ipv4 = Column(Ip)
+    ipv4 = Column(Ip, index=True)
     mask = Column(Integer, default=32)
 
     indicator_id = Column(Integer, ForeignKey('indicators.id', ondelete='CASCADE'))
@@ -133,7 +133,7 @@ class Ipv6(Base):
     __tablename__ = 'indicators_ipv6'
 
     id = Column(Integer, primary_key=True)
-    ip = Column(Ip(version=6))
+    ip = Column(Ip(version=6), index=True)
     mask = Column(Integer, default=64)
 
     indicator_id = Column(Integer, ForeignKey('indicators.id', ondelete='CASCADE'))
