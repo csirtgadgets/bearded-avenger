0d2fdd30f859dfb397562b84033ed12e88227ad1
Wes <wes@barely3am.com>
2016-12-22 15:12:11 -0500

    Fix/128 asnlookup (#207)
    * fixes #128
    
    * fixing indents
    
    * fixing nulls
    
    * work-around for asns that are ints and strings..
    
    * adding tests
    
    * fixing test

--- cif/gatherer/asn.py
+++ cif/gatherer/asn.py
@@ -1,63 +1,94 @@
+import logging
+import os
+import re
 
 from cif.utils import resolve_ns
-import logging
-import re
-from dns.rdtypes.ANY.TXT import TXT
+from cif.utils.asn_client import ASNClient
+
+ASN_FAST = os.environ.get('CIF_GATHERER_ASN_FAST')
 
 
 class Asn(object):
 
-    def __init__(self, *args, **kv):
+    def __init__(self, *args, **kwargs):
         self.logger = logging.getLogger(__name__)
+        self.asn_fast = kwargs.get('fast', ASN_FAST)
+        if self.asn_fast:
+            self.asn_fast = ASNClient(self.asn_fast)
 
     def _resolve(self, data):
         return resolve_ns('{}.{}'.format(data, 'origin.asn.cymru.com'), t='TXT')
 
+    def _resolve_fast(self, data):
+        return self.asn_fast.lookup(data)
+
     def process(self, indicator):
-        if indicator.itype == 'ipv4' and not indicator.is_private():
-            i = str(indicator.indicator)
-            match = re.search('^(\S+)\/\d+$', i)
-            if match:
-                i = match.group(1)
+        if indicator.is_private():
+            return
 
-            # cache it to the /24
-            # 115.87.213.115
-            # 0.213.87.115
-            i = list(reversed(i.split('.')))
-            i = '0.{}.{}.{}'.format(i[1], i[2], i[3])
+        # TODO ipv6
+        if indicator.itype != 'ipv4':
+            return
 
-            answers = self._resolve(i)
+        i = str(indicator.indicator)
+        match = re.search('^(\S+)\/\d+$', i)
+        if match:
+            i = match.group(1)
 
-            if len(answers) > 0:
-                # Separate fields and order by netmask length
-                # 23028 | 216.90.108.0/24 | US | arin | 1998-09-25
-                # 701 1239 3549 3561 7132 | 216.90.108.0/24 | US | arin | 1998-09-25
+        if ASN_FAST:
+            bits = self._resolve_fast(indicator.indicator)
+            for k in bits:
+                if bits[k] == 'NA':
+                    bits[k] = False
 
-                # i.asn_desc ????
-                self.logger.debug(answers[0])
-                bits = str(answers[0]).replace('"', '').strip().split(' | ')
-                asns = bits[0].split(' ')
+            if bits['asn']:
+                bits['asn'] = str(bits['asn'])
 
-                indicator.asn = asns[0]
-                indicator.prefix = bits[1]
-                indicator.cc = bits[2]
-                indicator.rir = bits[3]
-                answers = resolve_ns('as{}.{}'.format(asns[0], 'asn.cymru.com'), t='TXT', timeout=15)
+            indicator.asn = bits['asn']
+            indicator.prefix = bits['prefix']
+            indicator.asn_desc = bits['owner']
+            indicator.cc = bits['cc']
 
-                try:
-                    tmp = str(answers[0])
-                except UnicodeDecodeError as e:
-                    # requires fix latin-1 fix _escapeify to dnspython > 1.14
-                    self.logger.debug(e)
-                    return indicator
-                except IndexError:
-                    from pprint import pprint
-                    pprint(answers)
-                    return indicator
+            return indicator
 
-                bits = tmp.replace('"', '').strip().split(' | ')
-                if len(bits) > 4:
-                    indicator.asn_desc = bits[4]
+        # cache it to the /24
+        # 115.87.213.115
+        # 0.213.87.115
+        i = list(reversed(i.split('.')))
+        i = '0.{}.{}.{}'.format(i[1], i[2], i[3])
+
+        answers = self._resolve(i)
+
+        if len(answers) > 0:
+            # Separate fields and order by netmask length
+            # 23028 | 216.90.108.0/24 | US | arin | 1998-09-25
+            # 701 1239 3549 3561 7132 | 216.90.108.0/24 | US | arin | 1998-09-25
+
+            # i.asn_desc ????
+            self.logger.debug(answers[0])
+            bits = str(answers[0]).replace('"', '').strip().split(' | ')
+            asns = bits[0].split(' ')
+
+            indicator.asn = asns[0]
+            indicator.prefix = bits[1]
+            indicator.cc = bits[2]
+            indicator.rir = bits[3]
+            answers = resolve_ns('as{}.{}'.format(asns[0], 'asn.cymru.com'), t='TXT', timeout=15)
+
+            try:
+                tmp = str(answers[0])
+            except UnicodeDecodeError as e:
+                # requires fix latin-1 fix _escapeify to dnspython > 1.14
+                self.logger.debug(e)
+                return indicator
+            except IndexError:
+                from pprint import pprint
+                pprint(answers)
+                return indicator
+
+            bits = tmp.replace('"', '').strip().split(' | ')
+            if len(bits) > 4:
+                indicator.asn_desc = bits[4]
 
         # send back to router
         return indicator
---
+++ cif/utils/asn_client.py
@@ -0,0 +1,102 @@
+import json
+import logging
+import sys
+import zmq
+
+logger = logging.getLogger(__name__)
+
+
+class Timeout(Exception):
+    pass
+
+
+def chunk(it, slice=50):
+    """Generate sublists from an iterator
+    >>> list(chunk(iter(range(10)),11))
+    [[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]
+    >>> list(chunk(iter(range(10)),9))
+    [[0, 1, 2, 3, 4, 5, 6, 7, 8], [9]]
+    >>> list(chunk(iter(range(10)),5))
+    [[0, 1, 2, 3, 4], [5, 6, 7, 8, 9]]
+    >>> list(chunk(iter(range(10)),3))
+    [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]
+    >>> list(chunk(iter(range(10)),1))
+    [[0], [1], [2], [3], [4], [5], [6], [7], [8], [9]]
+    """
+
+    assert(slice > 0)
+    a=[]
+
+    for x in it:
+        if len(a) >= slice :
+            yield a
+            a=[]
+        a.append(x)
+
+    if a:
+        yield a
+
+
+class ASNClient:
+    def __init__(self, endpoint='tcp://localhost:5555'):
+        context = zmq.Context()
+        logger.debug("Connecting to asn lookup server")
+        socket = context.socket(zmq.DEALER)
+        socket.set(zmq.LINGER, 200)
+        socket.connect(endpoint)
+        self.socket = socket
+
+
+        self.get_fields()
+
+    def get_fields(self):
+        self.socket.send_string("fields")
+        if not self.socket.poll(timeout=3000):
+            raise Timeout()
+
+        self.fields = json.loads(self.socket.recv_string())
+        logger.debug("fields=%s", self.fields)
+
+    def lookup_many(self, ips):
+        outstanding = 0
+
+        for batch in chunk(ips, 100):
+            msg = ' '.join(batch)
+            self.socket.send_string(msg)
+            outstanding += 1
+            if outstanding < 10:
+                continue
+            if not self.socket.poll(timeout=3000):
+                raise Timeout()
+            response = self.socket.recv_string()
+            outstanding -=1
+            records = json.loads(response)
+            for rec in records:
+                yield dict(zip(self.fields, rec))
+
+        for _ in range(outstanding):
+            if not self.socket.poll(timeout=3000):
+                raise Timeout()
+            response = self.socket.recv_string()
+            outstanding -=1
+            records = json.loads(response)
+            for rec in records:
+                yield dict(zip(self.fields, rec))
+
+    def lookup(self, ip):
+        return next(self.lookup_many([ip]))
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    endpoint = 'tcp://localhost:5555'
+    if len(sys.argv) > 1:
+        endpoint = sys.argv[1]
+    c = ASNClient(endpoint)
+    ips = (line.rstrip() for line in sys.stdin)
+    for rec in c.lookup_many(ips):
+        print("\t".join(str(rec[f]) for f in c.fields))
+
+if __name__ == "__main__":
+    main()
