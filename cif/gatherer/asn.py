513ddd2b8e495d3eaa9afcecb4202a123cfadbf6
Wes Young <wes@barely3am.com>
2016-12-22 14:38:24 -0500

    fixing indents

--- cif/gatherer/asn.py
+++ cif/gatherer/asn.py
@@ -23,62 +23,69 @@ class Asn(object):
         return self.asn_fast.lookup(data)
 
     def process(self, indicator):
-        if indicator.itype == 'ipv4' and not indicator.is_private():
-            i = str(indicator.indicator)
-            match = re.search('^(\S+)\/\d+$', i)
-            if match:
-                i = match.group(1)
+        if indicator.is_private():
+            return
 
-            if ASN_FAST:
-                bits = self._resolve_fast(indicator.indicator)
-                for k in bits:
-                    if bits[k] == 'NA':
-                        bits[k] = ''
+        # TODO ipv6
+        if indicator.itype != 'ipv4':
+            return
+        
+        i = str(indicator.indicator)
+        match = re.search('^(\S+)\/\d+$', i)
+        if match:
+            i = match.group(1)
 
-                indicator.asn = str(bits['asn'])
-                indicator.prefix = bits['prefix']
-                indicator.asn_desc = bits['owner']
-                indicator.cc = bits['cc']
+        if ASN_FAST:
+            bits = self._resolve_fast(indicator.indicator)
+            for k in bits:
+                if bits[k] == 'NA':
+                    bits[k] = ''
 
-            else:
-                # cache it to the /24
-                # 115.87.213.115
-                # 0.213.87.115
-                i = list(reversed(i.split('.')))
-                i = '0.{}.{}.{}'.format(i[1], i[2], i[3])
+            indicator.asn = str(bits['asn'])
+            indicator.prefix = bits['prefix']
+            indicator.asn_desc = bits['owner']
+            indicator.cc = bits['cc']
 
-                answers = self._resolve(i)
+            return indicator
 
-                if len(answers) > 0:
-                    # Separate fields and order by netmask length
-                    # 23028 | 216.90.108.0/24 | US | arin | 1998-09-25
-                    # 701 1239 3549 3561 7132 | 216.90.108.0/24 | US | arin | 1998-09-25
+        # cache it to the /24
+        # 115.87.213.115
+        # 0.213.87.115
+        i = list(reversed(i.split('.')))
+        i = '0.{}.{}.{}'.format(i[1], i[2], i[3])
 
-                    # i.asn_desc ????
-                    self.logger.debug(answers[0])
-                    bits = str(answers[0]).replace('"', '').strip().split(' | ')
-                    asns = bits[0].split(' ')
+        answers = self._resolve(i)
 
-                    indicator.asn = asns[0]
-                    indicator.prefix = bits[1]
-                    indicator.cc = bits[2]
-                    indicator.rir = bits[3]
-                    answers = resolve_ns('as{}.{}'.format(asns[0], 'asn.cymru.com'), t='TXT', timeout=15)
+        if len(answers) > 0:
+            # Separate fields and order by netmask length
+            # 23028 | 216.90.108.0/24 | US | arin | 1998-09-25
+            # 701 1239 3549 3561 7132 | 216.90.108.0/24 | US | arin | 1998-09-25
 
-                    try:
-                        tmp = str(answers[0])
-                    except UnicodeDecodeError as e:
-                        # requires fix latin-1 fix _escapeify to dnspython > 1.14
-                        self.logger.debug(e)
-                        return indicator
-                    except IndexError:
-                        from pprint import pprint
-                        pprint(answers)
-                        return indicator
+            # i.asn_desc ????
+            self.logger.debug(answers[0])
+            bits = str(answers[0]).replace('"', '').strip().split(' | ')
+            asns = bits[0].split(' ')
 
-                    bits = tmp.replace('"', '').strip().split(' | ')
-                    if len(bits) > 4:
-                        indicator.asn_desc = bits[4]
+            indicator.asn = asns[0]
+            indicator.prefix = bits[1]
+            indicator.cc = bits[2]
+            indicator.rir = bits[3]
+            answers = resolve_ns('as{}.{}'.format(asns[0], 'asn.cymru.com'), t='TXT', timeout=15)
+
+            try:
+                tmp = str(answers[0])
+            except UnicodeDecodeError as e:
+                # requires fix latin-1 fix _escapeify to dnspython > 1.14
+                self.logger.debug(e)
+                return indicator
+            except IndexError:
+                from pprint import pprint
+                pprint(answers)
+                return indicator
+
+            bits = tmp.replace('"', '').strip().split(' | ')
+            if len(bits) > 4:
+                indicator.asn_desc = bits[4]
 
         # send back to router
         return indicator
