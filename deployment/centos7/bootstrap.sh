d1728803699fffb2c17060dd92608a00852c3a85
Wes Young <wes@barely3am.com>
2017-02-11 10:20:47 -0500

    cleanup

--- deployment/centos7/bootstrap.sh
+++ deployment/centos7/bootstrap.sh
@@ -7,17 +7,8 @@ set -e
 yum -y update
 
 echo 'updating apt-get tree and installing python-pip'
-sudo yum install -y gcc python-pip python-devel git libffi-devel openssl-devel python-virtualenv python-virtualenvwrapper
-
-selinuxenabled		# Check if selinux is enabled
-if [ $? -eq 0 ]; then	# Yes
-	sudo yum install -y libselinux-python
-elsif [ $? -eq 1 ]; then	# No
-	# NOP
-else  # unexpected condition
-	echo "Unexpected exit code from selinuxenabled ($?)"
-	exit 1
-fi
+sudo yum install -y gcc python-pip python-devel git libffi-devel openssl-devel python-virtualenv \
+    python-virtualenvwrapper libselinux-python
 
 echo 'installing ansible...'
 sudo pip install 'setuptools>=18.3,<34.0' 'ansible>=2.1,<3.0' versioneer markupsafe
@@ -25,35 +16,4 @@ sudo pip install 'setuptools>=18.3,<34.0' 'ansible>=2.1,<3.0' versioneer markups
 echo 'running ansible...'
 ansible-playbook -i "localhost," -c local localhost.yml -vv
 
-echo 'testing connectivity'
-sudo -u cif cif --config /home/cif/.cif.yml -p
-
-echo 'testing query'
-sudo -u cif cif --config /home/cif/.cif.yml --search example.com
-
-echo 'waiting...'
-sleep 5
-
-echo 'testing query'
-sudo -u cif cif --config /home/cif/.cif.yml --search example.com
-
-echo 'waiting...'
-sleep 5
-
-sudo -u cif cif --config /home/cif/.cif.yml --itype ipv4
-
-sudo -u cif cif --config /home/cif/.cif.yml -q 93.184.216.34
-
-echo 'waiting...'
-sleep 5
-
-sudo -u cif cif --config /home/cif/.cif.yml -q 93.184.216.34
-
-sudo systemctl stop csirtg-smrt.service
-
-sudo su - cif
-csirtg-smrt -r /etc/cif/rules/default/csirtg.yml -d --remember --client cif --config /etc/cif/csirtg-smrt.yml
-cif --config /home/cif/.cif.yml --provider csirtg.io
-exit
-
-sudo systemctl start csirtg-smrt.service
+bash ../test.sh
--- deployment/ubuntu16/bootstrap.sh
+++ deployment/ubuntu16/bootstrap.sh
@@ -2,10 +2,9 @@
 
 set -e
 
-#echo 'yes' | sudo add-apt-repository 'ppa:fkrull/deadsnakes-python2.7'
-
 echo 'updating apt-get tree and installing python-pip'
-sudo apt-get update && sudo apt-get install -y python2.7 python-pip python-dev git libffi-dev libssl-dev sqlite3 software-properties-common aptitude
+sudo apt-get update && sudo apt-get install -y python2.7 python-pip python-dev git libffi-dev libssl-dev sqlite3 \
+    software-properties-common aptitude
 
 echo 'Upgrading pip...'
 sudo -H pip install --upgrade pip
@@ -16,26 +15,4 @@ sudo -H pip install 'setuptools>=18.5,<34.0' 'ansible==2.1' versioneer markupsaf
 echo 'running ansible...'
 ansible-playbook -i "localhost," -c local localhost.yml -vv
 
-echo 'testing connectivity'
-sudo -u cif cif --config /home/cif/.cif.yml -p
-
-echo 'testing query'
-sudo -u cif cif --config /home/cif/.cif.yml --search example.com
-
-echo 'waiting...'
-sleep 5
-
-echo 'testing query'
-sudo -u cif cif --config /home/cif/.cif.yml --search example.com
-
-echo 'waiting...'
-sleep 5
-
-sudo -u cif cif --config /home/cif/.cif.yml --itype ipv4
-
-sudo -u cif cif --config /home/cif/.cif.yml -q 93.184.216.34
-
-echo 'waiting...'
-sleep 5
-
-sudo -u cif cif --config /home/cif/.cif.yml -q 93.184.216.34
+bash ../test.sh
