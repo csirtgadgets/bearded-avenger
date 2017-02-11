7bac6e28232651726295e14f24c888201a0ad64b
Wes Young <wes@barely3am.com>
2017-02-11 10:54:06 -0500

    fixing ubuntu16

--- deployment/ubuntu16/bootstrap.sh
+++ deployment/ubuntu16/bootstrap.sh
@@ -6,11 +6,8 @@ echo 'updating apt-get tree and installing python-pip'
 sudo apt-get update && sudo apt-get install -y python2.7 python-pip python-dev git libffi-dev libssl-dev sqlite3 \
     software-properties-common aptitude
 
-echo 'Upgrading pip...'
-sudo -H pip install --upgrade pip
-
 echo 'installing ansible...'
-sudo -H pip install 'setuptools>=18.5,<34.0' 'ansible==2.1' versioneer markupsafe
+sudo -H pip install 'setuptools>=18.5,<34.0' 'ansible>=2.1,<3.0' versioneer markupsafe
 
 echo 'running ansible...'
 ansible-playbook -i "localhost," -c local localhost.yml -vv
