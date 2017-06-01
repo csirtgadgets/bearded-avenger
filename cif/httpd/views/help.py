d61d1a962814ffe0f31c6c96987644fa820ae191
Wes <wesyoung@users.noreply.github.com>
2017-06-01 19:49:35 -0400

    fixes #308 (#312)
    * fixes #308
    
    * fixing peer tests

--- cif/httpd/views/help.py
+++ cif/httpd/views/help.py
@@ -11,12 +11,12 @@ class HelpAPI(MethodView):
             "GET /u": 'browser friendly ui [login with api token]',
             "GET /help/confidence": "get a list of the defined confidence values",
             'GET /ping': 'ping the router interface',
-            'GET /search': 'search for an indicator',
-            'GET /indicators': 'search for a set of indicators',
+            'GET /search?{q,limit,itype,indicator,confidence,tags,reporttime}': 'search for an indicator',
+            'GET /indicators?{q,limit,indicator,confidence,tags,reporttime}': 'search for a set of indicators',
             'POST /indicators': 'post indicators to the router',
-            'GET /feed': 'filter for a data-set, aggregate and apply respective whitelist',
-            'GET /tokens': 'search for a set of tokens',
+            'GET /feed?{q,limit,itype,confidence,tags,reporttime}': 'filter for a data-set, aggregate and apply respective whitelist',
+            'GET /tokens?{username,token}': 'search for a set of tokens',
             'POST /tokens': 'create a token or set of tokens',
-            'DELETE /tokens': 'delete a token or set of tokens',
+            'DELETE /tokens?{username,token}': 'delete a token or set of tokens',
             'PATCH /token': 'update a token'
         })
