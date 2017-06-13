646b70987c341a8f3e2f41ec5317dafff175d28b
Wes <wesyoung@users.noreply.github.com>
2017-06-13 14:04:55 -0400

    fixes #320 (#329)

--- cif/store/zelasticsearch/__init__.py
+++ cif/store/zelasticsearch/__init__.py
@@ -5,6 +5,9 @@ from cif.store.plugin import Store
 from cif.store.zelasticsearch.token import TokenManager
 from cif.store.zelasticsearch.indicator import IndicatorManager
 from elasticsearch_dsl.connections import connections
+from elasticsearch.exceptions import ConnectionError
+import traceback
+from time import sleep
 from cif.constants import TOKEN_CACHE_DELAY
 import arrow
 from cif.constants import PYVERSION
@@ -40,20 +43,48 @@ class _ElasticSearch(Store):
         if type(nodes) == str:
             nodes = nodes.split(',')
 
+        if not nodes:
+            nodes = ES_NODES
+
         self.indicators_prefix = kwargs.get('indicators_prefix', 'indicators')
         self.tokens_prefix = kwargs.get('tokens_prefix', 'tokens')
 
         logger.info('setting es nodes {}'.format(nodes))
+
         connections.create_connection(hosts=nodes)
 
+        self._alive = False
+
+        while not self._alive:
+            if not self.ping():
+                logger.warn('ES cluster not accessible')
+                logger.info('retrying connection in 30s')
+                sleep(30)
+
+            self._alive = True
+
+        logger.info('ES connection successful')
         self.tokens = TokenManager()
         self.indicators = IndicatorManager()
 
-    def ping(self, token):
+    def ping(self):
         # http://elasticsearch-py.readthedocs.org/en/master/api.html#elasticsearch.client.IndicesClient.stats
-        x = connections.get_connection().cluster.health()
+
+        try:
+            x = connections.get_connection().cluster.health()
+        except ConnectionError as e:
+            logger.warn('elasticsearch connection error')
+            logger.error(e)
+            return
+
+        except Exception as e:
+            logger.error(traceback.print_exc())
+            return
 
         if x['status'] in ['green', 'yellow']:
+            logger.info('ES cluster is: %s' % x['status'])
             return True
+
+        logger.warn('ES Cluster RED')
 
 Plugin = _ElasticSearch
