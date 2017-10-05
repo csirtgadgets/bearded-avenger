574fd75151436774c3380e36d18dd20b5102dcc4
Wes <wesyoung@users.noreply.github.com>
2017-10-05 12:21:51 -0400

    fixes #286 (#357)

--- test/test_gatherer_geo.py
+++ test/test_gatherer_geo.py
@@ -9,7 +9,7 @@ data = {
 }
 
 
-def test_gatherer_geo():
+def test_gatherer_geo_v4():
     a = Geo()
 
     def _resolve(i):
@@ -19,6 +19,23 @@ def test_gatherer_geo():
     a._resolve = _resolve
 
     i = Indicator(indicator='216.90.108.0')
+
+    a.process(i)
+
+    assert i.cc == data['cc']
+    assert i.city == data['city']
+
+
+def test_gatherer_geo_v6():
+    a = Geo()
+
+    def _resolve(i):
+        i.cc = data['cc']
+        i.city = data['city']
+
+    a._resolve = _resolve
+
+    i = Indicator(indicator='2607:ff10::c0:1:1:10d')
 
     a.process(i)
 
