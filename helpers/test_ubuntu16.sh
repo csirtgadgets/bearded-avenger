e1c4cc68b5a01ff3aec26f06a5d6e34950b84a6d
Wes Young <wes@barely3am.com>
2017-11-16 16:02:23 -0500

    helpers

---
+++ helpers/test_ubuntu16.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export VAGRANT_VAGRANTFILE=Vagrantfile
+export CIF_BOOTSTRAP_TEST=1
+export CIF_ANSIBLE_SDIST=/vagrant
+export CIF_HUNTER_THREADS=2
+export CIF_HUNTER_ADVANCED=1
+#export CIF_GATHERER_GEO_FQDN=1
+
+time vagrant up
---
+++ helpers/test_ubuntu16_es.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export VAGRANT_VAGRANTFILE=Vagrantfile
+export CIF_BOOTSTRAP_TEST=1
+export CIF_ANSIBLE_SDIST=/vagrant
+export CIF_HUNTER_THREADS=2
+export CIF_HUNTER_ADVANCED=1
+export CIF_ANSIBLE_ES=1
+#export CIF_GATHERER_GEO_FQDN=1
+
+time vagrant up
