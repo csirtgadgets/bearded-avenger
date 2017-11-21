178b8bb908fbbc74128d398a9c5b76e1c115cee6
Wes Young <wes@barely3am.com>
2017-11-21 11:20:25 -0500

    adding scaffolding for es-archiver

--- cif/utils/es_archiver.py
+++ cif/utils/es_archiver.py
@@ -9,13 +9,16 @@ from argparse import RawDescriptionHelpFormatter
 import logging
 import textwrap
 from elasticsearch import Elasticsearch
-from datetime import datetime, timedelta
+from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from elasticsearch import helpers
+from cif.store.zelasticsearch.indicator import Index, Indicator
+from cif.store.zelasticsearch.constants import WINDOW_LIMIT
 import re
-from cifsdk import VERSION
+from cif.constants import VERSION
 from cifsdk.utils import setup_logging
 from pprint import pprint
+import os
 
 CONFIDENCE = 50
 MONTHS = 12
@@ -40,6 +43,10 @@ def main():
     p.add_argument('-m', '--months', help='how many months ago to archive [default %(default)s]', default=MONTHS)
     p.add_argument('--dry-run', action="store_true", help='dry run, do not delete')
     p.add_argument('--nodes', default=['localhost:9200'])
+    p.add_argument('--limit', help='specify scroll batch limit [default %(default)s]', default=LIMIT)
+
+    if not os.getenv('CIF_ELASTICSEARCH_TEST', False) == '1':
+        raise SystemError('This has NOT been tested yet, remove this line to test at your own risk!')
 
     args = p.parse_args()
     setup_logging(args)
@@ -52,7 +59,7 @@ def main():
 
     es = Elasticsearch(args.nodes, timeout=120, max_retries=10, retry_on_timeout=True)
 
-    monthlies = es.indices.get_aliases(index='{}-*.*'.format(args.index_prefix)).keys()
+    monthlies = es.indices.get_alias(index='{}-*.*'.format('cif.indicators')).keys()
     to_archive = {}
     for m in monthlies:
         match = re.search(r"^cif\.indicators-(\d{4}\.\d{2})$", m)
@@ -64,18 +71,18 @@ def main():
     # http://elasticsearch-py.readthedocs.io/en/master/api.html#elasticsearch.Elasticsearch.delete_by_query
     # http://stackoverflow.com/questions/26808239/elasticsearch-python-api-delete-documents-by-query
 
+    pprint(to_archive)
     yearlies = ()
     for c in to_archive:
         logger.info('archiving: {}'.format(c))
-        if args.dry_run:
-            continue
 
-        i = 'cif.indicators-2017'
+        match = re.search(r"^cif\.indicators-(\d{4}).\d{2}$", c)
+        i = 'cif.indicators-' + str(match.group(1))
+        logger.debug(i)
         # check to see if yearly bucket exists?
-        if not es.indices.exists(index='cif.indicators-2017'):
+        if not es.indices.exists(index=i):
             logger.debug("building: %s" % i)
-            from cif.store.zelasticsearch.indicator import Index, Indicator
-            from cif.store.zelasticsearch.constants import WINDOW_LIMIT
+
             idx = Index(i)
             idx.aliases(live={})
             idx.doc_type(Indicator)
@@ -88,31 +95,54 @@ def main():
         # aggregate index into yearly bucket
         # based on provider, tags(?), indicator
         data = ()
-        for d in elasticsearch.helpers.scroll(es, scroll='60m', size=5000):
-            i = d['indicator']
-            if d['indicator'] not in data:
-                data[i] = d
-                continue
+        for d in elasticsearch.helpers.scroll(es, scroll='60m', size=args.limit):
+            i = (d['indicator'], d['provider'], data['group'], sorted(d['tags']).join(','))
 
-            i = data[i]
-            i['count'] += 1
-            if i['lasttime'] < d['lasttime']:
-                i['lasttime'] = d['lasttime']
+            if i not in data:
+                data[i].add(d)
+            else:
+                i = data[i]
+                i['count'] += d['count']
 
-            if i['reporttime'] > d['reporttime']:
-                i['reporttime'] = d['reporttime']
+                if i['lasttime'] < d['lasttime']:
+                    i['lasttime'] = d['lasttime']
 
-            if i['firsttime'] > d['firsttime']:
-                i['firsttime'] = d['firsttime']
+                if i['reporttime'] > d['reporttime']:
+                    i['reporttime'] = d['reporttime']
 
-            # tags?
+                if i['firsttime'] > d['firsttime']:
+                    i['firsttime'] = d['firsttime']
 
-        # remove index
-        es.indices.delete(index=c, wait_for_completion=True)
+                if not i['message']:
+                    i['message'] = []
+
+                if d['message']:
+                    i['message'].append(d['message'])
+
+        if len(data) == 0:
+            logger.info('nothing to archive...')
+            continue
+
+        actions = [{'_index': 'cif.indicators-2017', '_type': 'indicator', '_source': d} for d in data]
+
+        # add to yearly
+        if not args.dry_run:
+            helpers.bulk(es, actions)
+
+        logger.debug('flushing...')
+        if es.flush():
+            logger.debug('removing %s' % c)
+            # remove old index
+            if not args.dry_run:
+                es.indices.delete(index=c, wait_for_completion=True)
 
     # optimize yearlies
     for y in yearlies:
-        es.indices.optimize(index=y)
+        logger.debug('optimizing: %s' % y)
+        if not args.dry_run:
+            es.indices.optimize(index=y)
+
+        logger.debug('optimized: %s' % y)
 
 
 if __name__ == "__main__":
