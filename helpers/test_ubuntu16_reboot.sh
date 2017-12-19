b5188ab50586100988dad397f25939a859c7bd5c
Wes Young <wes@barely3am.com>
2017-12-19 11:08:22 -0500

    adding helper

---
+++ helpers/test_ubuntu16_reboot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export VAGRANT_VAGRANTFILE=Vagrantfile
+export CIF_BOOTSTRAP_TEST=1
+export CIF_ANSIBLE_SDIST=/vagrant
+export CIF_HUNTER_THREADS=2
+export CIF_HUNTER_ADVANCED=1
+#export CIF_GATHERER_GEO_FQDN=1
+
+time vagrant up
+
+vagrant reload
+vagrant ssh -c 'cd /vagrant/deploymentkit && bash /vagrant/deploymentkit/test.sh'
