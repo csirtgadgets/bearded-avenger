baf6b9a29ac8d5de9f132318f81be11519f3ee89
Michael Davis <mdavis332@gmail.com>
2020-10-08 15:37:29 -0500

    Checks timestamps prior to indicator create (#492)
    Moves call to fix timestamps up one level so it applies to all backend store types

--- cif/store/indicator_plugin.py
+++ cif/store/indicator_plugin.py
@@ -26,16 +26,6 @@ class IndicatorManagerPlugin(object):
         if i['group'] not in t['groups']:
             raise AuthError('unable to write to %s' % i['group'])
 
-    def _timestamps_fix(self, i):
-        if not i.get('lasttime'):
-            i['lasttime'] = arrow.utcnow().datetime
-
-        if not i.get('firsttime'):
-            i['firsttime'] = i['lasttime']
-
-        if not i.get('reporttime'):
-            i['reporttime'] = arrow.utcnow().datetime
-
     def _is_newer(self, i, rec):
         if not i.get('lasttime'):
             return False
